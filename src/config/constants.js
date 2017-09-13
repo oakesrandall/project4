65ab77ebe1591635d643918d07c1b7a5923f5c89
David Randall <oakesrandall@gmail.com>
2017-09-13 13:24:27 -0600

    Firebase Auth working

---
+++ src/config/constants.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAP-MzzO6Yzl_tHYAdaVaTtdIPK2YLloxI",
+  authDomain: "dubow-house-project-management.firebaseapp.com",
+  databaseURL: "https://dubow-house-project-management.firebaseio.com",
+  projectId: "dubow-house-project-management",
+  storageBucket: "dubow-house-project-management.appspot.com",
+  messagingSenderId: "973562385261",
+};
+
+firebase.initializeApp(config);
+
+export const ref = firebase.database().ref();
+export const firebaseAuth = firebase.auth;
